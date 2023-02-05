45f5ecf6c4e361323994bcaf936013166ca9d632
Saurabh Khatiwada <khatiwada.saurabh@gmail.com>
2023-02-04 21:39:21 -0500

    Updates

---
+++ precache-manifest.77bce632413f8ea6ff261981c47d975c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1542e98d2488bdab0e9d8823fade3154",
+    "url": "/index.html"
+  },
+  {
+    "revision": "637ea9c071601b976c3e",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "795b4b8e632db6949e50",
+    "url": "/static/css/main.b3d67c7c.chunk.css"
+  },
+  {
+    "revision": "637ea9c071601b976c3e",
+    "url": "/static/js/2.272989d2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.272989d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "795b4b8e632db6949e50",
+    "url": "/static/js/main.9b489cc8.chunk.js"
+  },
+  {
+    "revision": "66f8d4971f5a2129cee7",
+    "url": "/static/js/runtime-main.84d4f96c.js"
+  },
+  {
+    "revision": "b262adb0e403195c07d3f199fa2df6de",
+    "url": "/static/media/logo.b262adb0.png"
+  }
+]);
