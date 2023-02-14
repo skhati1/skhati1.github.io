bf615514f4d90b8655f3c22cea863ed01b85b68e
Saurabh Khatiwada <khatiwada.saurabh@gmail.com>
2023-02-13 20:42:36 -0500

    Remove some packages + fix index html

--- src/App.js
+++ src/App.js
@@ -11,11 +11,6 @@ import {Helmet} from "react-helmet";
 function App() {
   return (
     <div className="App">
-      <Helmet>
-        <meta property="og:title" content="Khatiwada.DEV Title" />
-        <meta property="og:description" content="Khatiwada.DEV Description" />
-        <meta property="og:image" content="https://media.licdn.com/dms/image/C4D03AQGHwXKB6mbaJQ/profile-displayphoto-shrink_800_800/0/1516617257332?e=2147483647&v=beta&t=2esGtJ1kcQIiVwEeSGwzVRX4d5CIcBp_-L_bnoOAzco" />
-            </Helmet>
       <Container>
         <Row>
           <Col sm>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Tab, ListGroup, Row, Col } from 'react-bootstrap';
-import {Button} from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 
 export default function Projects() {
     const contentTextAlign = {
@@ -10,26 +10,23 @@ export default function Projects() {
         <div>
             <h4>Projects</h4>
             <hr />
-            <Tab.Container id="list-group-tabs-example" defaultActiveKey="#bbbautoschool">
+            <Tab.Container id="list-group-tabs-example" defaultActiveKey="#helloCloud">
                 <Row>
                     <Col sm={4}>
                         <ListGroup>
-                            
                             <ListGroup.Item action href="#helloCloud">
                                 Hello Cloud
                             </ListGroup.Item>
                         </ListGroup>
                     </Col>
-
-<Col sm={8}>
-                         <Tab.Content style={contentTextAlign}>
-                             
-                             <Tab.Pane eventKey="#helloCloud">
-                                 <p>A hands-on workshop designed for students of Towson University, along with <a href="https://www.codebyalex.com/" target="_blank" rel="noopener noreferrer">Alex Wilson</a>, about the basics of cloud development using AWS with an emphasis on development in conjunction with DevOps.</p>
-                                 <a href="https://github.com/CodeByAlex/HelloCloud"><Button variant="outline-info">Starter Code</Button></a>
-                             </Tab.Pane>
-                         </Tab.Content>
-                     </Col>
+                    <Col sm={8}>
+                        <Tab.Content style={contentTextAlign}>
+                            <Tab.Pane eventKey="#helloCloud">
+                                <p>A hands-on workshop designed for students of Towson University, along with <a href="https://www.codebyalex.com/" target="_blank" rel="noopener noreferrer">Alex Wilson</a>, about the basics of cloud development using AWS with an emphasis on development in conjunction with DevOps.</p>
+                                <a href="https://github.com/CodeByAlex/HelloCloud"><Button variant="outline-info">Starter Code</Button></a>
+                            </Tab.Pane>
+                        </Tab.Content>
+                    </Col>
 
                 </Row>
             </Tab.Container>
